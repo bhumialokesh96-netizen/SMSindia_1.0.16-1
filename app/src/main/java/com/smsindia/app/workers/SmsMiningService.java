2d758c8c4d5608ee720d07a6d18012e3edbf4515
bhumialokesh96-netizen <bhumialokesh96@gmail.com>
2025-12-05 23:46:45 +0530

    update SmsMiningService.java

--- app/src/main/java/com/smsindia/app/workers/SmsMiningService.java
+++ app/src/main/java/com/smsindia/app/workers/SmsMiningService.java
@@ -2,8 +2,8 @@ package com.smsindia.app.workers;
 
 import android.app.Activity;
 import android.app.Notification;
-import com.smsindia.app.services.SupabaseApi;
-import com.smsindia.app.services.TaskModel;
+import com.smsindia.app.service.SupabaseApi;
+import com.smsindia.app.service.TaskModel;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
